31f0c8920ccf9a034bafe0b694721cafcb995cb8
KAVYA KANKONKAR <163013825+KavyaKankonkar@users.noreply.github.com>
2026-07-14 08:03:07 +0530

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # grid_copy=deepcopy(grid)
+
+        rows=len(grid)
+        cols=len(grid[0])
+        perim=0
+        for i in range(rows):
+            for j in range(cols):
+                v=4
+                if grid[i][j] ==1:
+                    for dx,dy in [(0,1),(0,-1),(-1,0),(1,0)]:
+                        new_i= i+dx
+                        new_j= j+dy         
+                        if new_i<0 or new_i==rows or new_j<0 or new_j==cols:
+                            continue
+                        if grid[new_i][new_j] ==1 :
+                            v-=1
+                    perim+=v
+        return perim
+
